789a492f7547847522c457b5dab08ce0b2445c84
a733181 <a733181@gmail.com>
2023-06-13 21:13:25 +0800

    新增 radial_scale

---
+++ continuous_scales/radial_scale.js
@@ -0,0 +1,8 @@
+// d3.scaleRadial()
+
+const radialScale = d3.scaleRadial();
+console.log(radialScale(2));
+console.log(radialScale(4));
+
+console.log(radialScale.domain());
+console.log(radialScale.range());
