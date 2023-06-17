117df3fd7efde56688847480eecf037318fb4ce2
a733181 <a733181@gmail.com>
2023-06-17 15:21:23 +0800

    新增 project_cement_color

---
+++ sequential_scales/project_cement_color.js
@@ -0,0 +1,33 @@
+document.querySelector('button').addEventListener('click', () => {
+  const cement = parseInt(document.querySelector('#cement').value);
+  const blast = parseInt(document.querySelector('#blast').value);
+  const ash = parseInt(document.querySelector('#ash').value);
+  const age = parseInt(document.querySelector('#age').value);
+
+  const cementColor = d3
+    .scaleSequential()
+    .domain([101, 1425])
+    .interpolator(d3.interpolateGreys);
+
+  const domainValue = cement + blast + ash + age;
+
+  if (
+    cement < 100 ||
+    cement > 500 ||
+    blast < 0 ||
+    blast > 360 ||
+    ash < 0 ||
+    ash > 200 ||
+    age < 1 ||
+    age > 365 ||
+    domainValue === 0
+  ) {
+    return;
+  } else {
+    d3.select('svg rect').attr('fill', cementColor(domainValue));
+    d3.select('svg text')
+      .text(cementColor(domainValue))
+      .attr('x', '10')
+      .attr('y', '480');
+  }
+});
